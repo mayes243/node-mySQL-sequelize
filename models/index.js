0cd581e86c397e0aa8ea9a9d6fb1c7dbfd614ab8
mayes243 <mayeskhatri243@gmail.com>
2023-03-03 10:30:53 +0530

    code modified

--- models/index.js
+++ models/index.js
@@ -1,23 +1,21 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const dbConfig = require('../config/dbConfig.js');
+const env = process.env.NODE_ENV || "development";
+const dbConfig = require("../config/dbconfig.json")[env];
 
-const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
+const sequelize = new Sequelize({
+    ...dbConfig,
     // operatorsAliases : 0 => Remove the unwanted console
     operatorsAliases: 0, // true and false also acceptable
 
+    // CONNECTION LIMITATION (OPTIONAL)
     pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
+        max: 5, // maximum
+        min: 0, // minimun
+        acquire: 30000, // time acquired
+        idle: 10000 // idle time for database
     }
-})
+});
 
 sequelize.authenticate()
     .then(() => console.log("Mongodb connection SUCCESS ⭐"))
@@ -25,14 +23,14 @@ sequelize.authenticate()
 
 const db = {}
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 // Tables
 db.User = require('./userModel.js')(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false })
     .then(() => console.log('yes re-sync done! ✅'))
-    .catch(err => console.error('Unable to re-sync table: ❌', err));
+    .catch(err => console.log('Unable to re-sync table: ❌', err));
 
-module.exports = db; 
+module.exports = db;
